113b587697b5679ecb3f8ca9f705212ad6f90b58
bukowskiezequiel <60674763+bukowskiezequiel@users.noreply.github.com>
2020-02-19 22:13:57 -0300

    Add files via upload
    "miercoles 19-2"

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,12 +1,35 @@
 function mostrar()
 {
-
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
+//la diferencia de color es porque 1-4 no se usaron
 	
-	var respuesta='si';
+	var flag=0;
+	var positivo=0;
+	var negativo=1;  
+	var respuesta='s';
+	var numero;
+	//is nan devuelve que es verdadero
 
+	do {
+		 numero = parseInt(prompt("ingrese un numero: "));
+
+		 while (isNaN(numero)) {
+			 numero = parseInt(prompt("eso no es un numero. Ingrese un numero: "));
+		 }
+
+		 if(numero >= 0){
+			 positivo = positivo + numero;
+
+		 }else {
+			 negativo = negativo * numero;
+			 flag = 1;
+		 }
+		
+		respuesta = prompt(" quiere continuar ingresando numeros?")
+	} while (respuesta == "s");
+
+	if (flag == 0 ) {
+		negativo = 0
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,40 @@
 function mostrar()
+
+//Al presionar el botón pedir números hasta que el usuario quiera, 
+//mostrar el número máximo y el número mínimo ingresado.
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var flag=0;
+	var maximo;
+	var minimo;
+	var numero;
+	var respuesta;
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
-
+	do {//pido numero al usuario
+		numero = parseInt(prompt(" Ingrese un numero"));
+		//valido para que sea un numero
+		while (isNaN(numero)){
+			numero = parseInt(prompt("eso no es un numero. Ingrese un numero: "));
+		}
+		//me fijo si es maximo o minimo
+        if (flag == 0 ){
+			maximo = numero;
+			minimo = numero;
+			flag = 1;
+		}else{
+			if (numero > maximo){
+				maximo = numero;
+			}
+			if (numero < minimo){
+				minimo = numero;
+			}
+		}
+		//pregunto si quiere continuar
+		respuesta = prompt("quiere ingresar otro numero?")
+	}while(respuesta!='n')
+	//muestro la informacion
+document.getElementById("maximo").value = maximo
+document.getElementById("minimo").value = minimo
 
 
 
